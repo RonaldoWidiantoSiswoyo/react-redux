e6a5efe1e667f744d6c57e6855d99384e3c8bb3f
RonaldoWidiantoSiswoyo <ronaldowidianto123@gmail.com>
2022-07-22 19:31:58 +0700

    first commit

---
+++ src/redux.js
@@ -0,0 +1,44 @@
+// import { createStore } from "redux";
+import { Reducer } from "redux";
+import { ActionFromReducer } from "redux";
+import { ReducerState } from "react";
+
+const redux = require("redux");
+const createStore = redux.createStore;
+
+const initialState = {
+  value: 0,
+  age: 17,
+};
+
+//Reducer
+const rootReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "ADD-_AGE":
+      return {
+        ...state,
+        age: state.age + 1,
+      };
+    case "CHANGE_VALUE":
+      return {
+        ...state,
+        value: state.value + action.value,
+      };
+    default:
+      return state;
+  }
+};
+
+// Store
+const store = createStore(rootReducer);
+console.log(store.getState());
+
+// Subscription
+store.subscribe(() => {
+  console.log("store change : " + store.getState());
+});
+
+// Dispatching Action
+store.dispatch({ type: "ADD_AGE" });
+store.dispatch({ type: "CHANGE_VALUE", newValue: 12 });
+console.log(store.getState());
